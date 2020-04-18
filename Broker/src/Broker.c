1d5c8f1ba5f5d9b5ba59ec5e12a606b91112e1c1
gonzalemi <gonzalemi@gmail.com>
2020-04-18 19:30:18 -0300

    estructura broker

---
+++ Broker/src/Broker.c
@@ -0,0 +1,21 @@
+/*
+ ============================================================================
+ Name        : Broker.c
+ Author      : 
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <commons/string.h>
+
+int main(void) {
+	char* string = string_new();
+	string_append(&string, "!!!Hello World Broker!!!");
+
+	puts(string); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
