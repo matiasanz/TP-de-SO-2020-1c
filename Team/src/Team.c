d92eae7bdfb97bee3596e83b393e14a23db71787
GustavoChoque <e_gustavo14@hotmail.com>
2020-05-03 14:30:46 -0300

    agrego funcion inicializar con config y log

--- Team/src/Team.c
+++ Team/src/Team.c
@@ -14,6 +14,38 @@
 int main(void) {
 
 	printf("hola soy el team");
+
+	if(inicializar()==-1){
+		exit_team();
+		return EXIT_FAILURE;
+	};
+
 	return EXIT_SUCCESS;
 }
 
+int inicializar(){
+
+	config=config_create(CONFIG_PATH);
+
+	logger=log_create(config_get_string_value(config,"LOG_FILE"),TEAM_STRING,true,LOG_LEVEL_INFO);
+
+	subscribpcion_colas();
+
+	return 1;
+}
+
+int subscribpcion_colas(){
+
+	//TO DO//
+
+	return 1;
+}
+
+
+
+void exit_team(){
+
+	log_destroy(logger);
+	config_destroy(config);
+
+}
--- Team/src/team.h
+++ Team/src/team.h
@@ -11,4 +11,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <commons/config.h>
+#include <commons/log.h>
+
+#include <crenito-commons/conexiones/conexiones.h>
+#include <crenito-commons/utils.h>
+
+
+/*Constantes*/
+#define CONFIG_PATH "config/team.config"
+
+
+/*Variables Globales*/
+
+t_config* config;
+
+t_log* logger;
+/*--------------*/
+
+
+int inicializar();
+int subscribpcion_colas();
+
+void exit_team();
+
+
+
 #endif /* SRC_TEAM_H_ */
