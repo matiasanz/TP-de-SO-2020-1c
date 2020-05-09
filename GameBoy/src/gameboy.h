eaf028ffacc3ffe696c1bdf0351d819990a0748e
Gaston-Erazo18 <gastonerazo018@gmail.com>
2020-05-09 20:34:49 -0300

    primer commit

--- GameBoy/src/gameboy.h
+++ GameBoy/src/gameboy.h
@@ -1,15 +1,15 @@
-/*
- * gameboy.h
- *
- *  Created on: 2 may. 2020
- *      Author: utnso
- */
-
-#ifndef SRC_GAMEBOY_H_
-#define SRC_GAMEBOY_H_
+#ifndef GAMEBOY_H_
+#define GAMEBOY_H_
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <crenito-commons/utils.h>
+#include <crenito-commons/mensajes/mensaje_new_pokemon.h>
+#include <crenito-commons/conexiones/socket.h>
+#include <commons/log.h>
+#include <commons/config.h>
 
 
-#endif /* SRC_GAMEBOY_H_ */
+
+
+#endif
