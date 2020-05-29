b83986f23ab1ae093aefd26ddbb273a5664b7b8e
Gaston-Erazo18 <49112200+Gaston-Erazo18@users.noreply.github.com>
2020-05-29 03:23:31 -0300

    prueba

--- GameBoy/src/GameBoy.c
+++ GameBoy/src/GameBoy.c
@@ -9,4 +9,22 @@
  */
 
 
+inicializar_logs();
 
+	//Me traigo la config para conocer la ip puerto del broker
+	t_config* config = config_create("./config/gameboy.config");
+
+	conexion_broker = conexion_server_crear(
+				config_get_string_value(config, "IP_BROKER"),
+				config_get_string_value(config, "PUERTO_BROKER"), GAMEBOY,
+				0);
+
+	return 1;
+
+}
+
+void inicializar_logs() {
+
+	logger = log_create("./log/gameboy.log", GAMEBOY_STRING, 1, LOG_LEVEL_INFO);
+	event_logger = log_create("./log/gameboy_event.log", "GAMEBOY_EVENT", 1, LOG_LEVEL_INFO);
+}
