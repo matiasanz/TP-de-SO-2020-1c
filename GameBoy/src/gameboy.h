80570a474ae5757e860427cbc1ff3fe33855112f
Gaston-Erazo18 <gastonerazo018@gmail.com>
2020-05-29 00:30:32 -0300

    prueba

--- GameBoy/src/gameboy.h
+++ GameBoy/src/gameboy.h
@@ -1,24 +1,7 @@
 #ifndef GAMEBOY_H_
 #define GAMEBOY_H_
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <crenito-commons/utils.h>
-#include <crenito-commons/mensajes/mensaje_new_pokemon.h>
-#include <crenito-commons/mensajes/mensaje_appeared_catch_pokemon.h>
-#include <crenito-commons/mensajes/mensaje_caught_pokemon.h>
-#include <crenito-commons/mensajes/mensaje_get_pokemon.h>
-#include <crenito-commons/mensajes/mensaje_localized_pokemon.h>
-#include <crenito-commons/conexiones/socket.h>
-#include <commons/log.h>
-#include <commons/config.h>
-#include <crenito-commons/conexiones/conexiones.h>
 
-t_config* config;
-t_log* logger;
-t_log* event_logger;
-
-void inicializar_logs(void);
 
 
 #endif
