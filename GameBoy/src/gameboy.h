d7ed7b258155709421ad58facd229be9cba110b7
Gaston-Erazo18 <49112200+Gaston-Erazo18@users.noreply.github.com>
2020-05-29 03:24:38 -0300

    prueba

--- GameBoy/src/gameboy.h
+++ GameBoy/src/gameboy.h
@@ -1,7 +1,13 @@
-#ifndef GAMEBOY_H_
-#define GAMEBOY_H_
+#ifndef SRC_GAMEBOY_H_
+#define SRC_GAMEBOY_H_
 
+#include "crenito-commons/conexiones/conexiones.h"
 
+t_conexion_server conexion_broker;
+
+void inicializar_logs();
+
+#endif /* SRC_GAMEBOY_H_ */
 
 
 #endif
