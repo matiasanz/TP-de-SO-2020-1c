538ae80f8ecb1c2a317f3ae2e6304792cd963b07
gonzalemi <gonzalemi42@gmail.com>
2020-06-02 00:21:36 -0300

    fix headers de funciones

--- CrenitoCommons/src/crenito-commons/conexiones/conexiones.h
+++ CrenitoCommons/src/crenito-commons/conexiones/conexiones.h
@@ -11,41 +11,40 @@
 #include "conexiones-utils.h"
 
 /*
- * Funcion utilizada por los procesos GAME_BOY, GAME_CARD y GAME_BOY para
+ * Funcion utilizada por los procesos GAME_BOY, GAME_CARD y TEAM para
  * enviar mensajes a un servidor dados un IP y un PUERTO.
  * La funcion crea un socket cliente, envia el paquete y luego cierra el socket.
  * En caso de error devuelve ERROR_SOCKET
- * Esta funcion debe usarse desde GameBoyTeam, GameCard y Team.
- *
+ * 
  */
 int enviar(t_conexion_server* server, t_paquete* pqt);
 
 /*
  * Envia un paquete al socket que recibe por parámetro.
- * IMPORTANTE: Esta funcion debe usarse unicamente desde Broker
+ * IMPORTANTE: Esta funcion debe usarse unicamente desde el BROKER
  * ya que es el único proceso que mantiene el socket abierto luego del envío.
  * Los demás procesos deben usar la funcion enviar(..) definida arriba.
  */
 int enviar_paquete(t_paquete* pqt, int socket);
 
 /*
- * Funcion utilizada por los procesos GAME_BOY, GAME_CARD y GAME_BOY para
+ * Funcion utilizada por los procesos GAME_CARD y TEAM para
  * subscribirse a una cola de mensajes.
- * Luego de subscribir al proceso, se queda esuchando mensajes en dicha cola 
+ * Luego de subscribir, se queda esuchando mensajes en dicha cola 
  * (escucha activa).
  * Al recibir un mensaje lo reenvia a través de la funcion de escucha que
  * recibe por parámeetro llamada mensaje_recibido(...)
- * Los procesos que usen esta funcion deben definir la función de escucha.
  */
 void subscribir_y_escuchar_cola(t_conexion* args);
 
 /*
+ * Funcion utilizada por los procesos GAME_CARD y TEAM para
+ * conectarse con el GAME_BOY
  * Crea un socket de escucha usando un ip y puerto que recibe desde
  * el archivo de configuración.
- * Luego realiza escucha activa de mensajes (escucha activa).
- *  Al recibir un mensaje lo reenvia a través de la funcion de escucha que
+ * Luego realiza escucha activa de mensajes.
+ * Al recibir un mensaje lo reenvia a través de la funcion de escucha que
  * recibe por parámeetro llamada mensaje_recibido(...)
- * Los procesos que usen esta funcion deben definir la función de escucha.
  */
 void conectar_y_escuchar_gameboy(t_conexion_host* gameboy);
 
