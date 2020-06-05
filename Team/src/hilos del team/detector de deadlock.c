2df6f81fa50f2a0e7982cf2097d9add377f0be83
matiasanz <matias.l.anzorandia@gmail.com>
2020-06-05 03:41:36 -0300

    El algoritmo de deadlock retorna a los entrenadores que detecto y se paso a su archivo

--- Team/src/hilos del team/detector de deadlock.c
+++ Team/src/hilos del team/detector de deadlock.c
@@ -5,9 +5,8 @@
 
 #define TIEMPO_ENTRE_DETECCIONES 15 //segundos
 
-pares_intercambio intercambiosPendientes;
-
 candidatos_intercambio entrenadoresConRecursosEnExceso;
+pares_intercambio intercambiosPendientes;
 
 //Es un gran TODO
 void team_ejecutar_algoritmo_de_deteccion_de_deadlock(){
@@ -38,91 +37,105 @@ void team_ejecutar_algoritmo_de_deteccion_de_deadlock(){
  *  L   D ; (libero)   (dejo)
  *
  */
-//bool algoritmo_detectar_deadlock(entrenadores unEquipo, entrenadores enDeadlock){
-//
-//	matriz_recursos necesidad = entrenadores_pedidos_insatisfechos(unEquipo);
-//	matriz_recursos disponibles = entrenadores_recursos_sobrantes(unEquipo);
-//
-//	bool hayDeadlock = hay_espera_circular(unEquipo, necesidad, disponibles)
-//		|| hay_deadlock_exceptuando_al_siguiente(unEquipo, necesidad, disponibles, enDeadlock)
-//		|| hay_deadlock_dejando_al_siguiente(unEquipo, necesidad, disponibles, enDeadlock);
-//
-//	recursos_destroy(necesidad);
-//	recursos_destroy(disponibles);
-//
-//	return hayDeadlock;
-//}
-//
-//bool hay_deadlock_exceptuando_al_siguiente(entrenadores unEquipo, matriz_recursos necesidad, matriz_recursos disponibles, entrenadores enDeadlock){
-//
-//	entrenadores procesos = unEquipo;
-//	entrenadores procesosEnDeadlock = list_duplicate(enDeadlock);
-//
-//	entrenador*unEntrenador = list_remove(procesos, 0);
-//
-//	entrenadores aEvaluar = list_duplicate(procesos);
-//	list_add_all(aEvaluar, procesosEnDeadlock);
-//
-//	if(!unEntrenador){
-//		puts("L-NULL");
-//		return false;
-//	}
-//
-//	printf("L%d\n", unEntrenador->id);
-//
-//	matriz_recursos necesidadActual = recursos_matriz_diferencia(necesidad, entrenador_recursos_pedidos(unEntrenador));
-//	matriz_recursos disponiblesActuales = recursos_matriz_diferencia(disponibles, entrenador_recursos_sobrantes(unEntrenador));
-//
-//	bool deadlock = hay_espera_circular(aEvaluar, necesidadActual, disponiblesActuales)
-//		|| hay_deadlock_exceptuando_al_siguiente(procesos, necesidadActual, disponiblesActuales, procesosEnDeadlock)
-//		|| hay_deadlock_dejando_al_siguiente(procesos, necesidadActual, disponiblesActuales, procesosEnDeadlock);
-//
-//	list_add_in_index(procesos, 0, unEntrenador);
-//
-//	recursos_destroy(necesidadActual);
-//	recursos_destroy(disponiblesActuales);
-//
-//	return deadlock;
-//}
-//
-//bool hay_deadlock_dejando_al_siguiente(entrenadores unEquipo, matriz_recursos necesidad, matriz_recursos disponibles, entrenadores enDeadlock){
-//
-//	entrenadores procesos = unEquipo;//esAca list_duplicate(unEquipo);
-//	entrenadores procesosEnDeadlock = list_duplicate(enDeadlock);
-//
-//	entrenador*unEntrenador = list_remove(procesos, 0);
-//	list_add(procesosEnDeadlock, unEntrenador);
-//
-//	if(!unEntrenador){
-////		puts("D-NULL");
-//		return false;
-//	}
-//
-//	printf("D%d\n", unEntrenador->id);
-//
-//	entrenadores aEvaluar = list_duplicate(procesos);
-//	list_add_all(aEvaluar, procesosEnDeadlock);
-//
-//	bool deadlock = hay_espera_circular(aEvaluar, necesidad, disponibles)
-//		|| hay_deadlock_exceptuando_al_siguiente(procesos, necesidad, disponibles, procesosEnDeadlock);
-//
-//	if(!deadlock){
-//		printf("********retracto N°%d\n", unEntrenador->id);
-//		deadlock = deadlock || hay_deadlock_dejando_al_siguiente(procesos, necesidad, disponibles, procesosEnDeadlock);
-//	}
-//
-//	//esAca
-//	list_add_in_index(procesos, 0, unEntrenador);
-//
-//	return deadlock;
-//}
-//
-//bool hay_espera_circular(entrenadores unEquipo, matriz_recursos necesidad,matriz_recursos disponibles){
-////	printf("Necesidad:"); recursos_mostrar(necesidad); puts("");
-////	printf("Disponibles:"); recursos_mostrar(disponibles);puts("");
-//	return recursos_suficientes_para(necesidad, disponibles) && list_size(unEquipo)>1;
-//}
-//
+bool algoritmo_detectar_deadlock(entrenadores unEquipo, entrenadores enDeadlock){
+
+	matriz_recursos necesidad = entrenadores_pedidos_insatisfechos(unEquipo);
+	matriz_recursos disponibles = entrenadores_recursos_sobrantes(unEquipo);
+
+	entrenadores omitidos = entrenadores_create();
+
+	bool hayDeadlock = hay_espera_circular(unEquipo, necesidad, disponibles)
+		|| hay_deadlock_exceptuando_al_siguiente(unEquipo, necesidad, disponibles, omitidos, enDeadlock)
+		|| hay_deadlock_dejando_al_siguiente(unEquipo, necesidad, disponibles, omitidos, enDeadlock);
+
+	recursos_destroy(necesidad);
+	recursos_destroy(disponibles);
+	list_destroy(omitidos);
+
+	return hayDeadlock;
+}
+
+bool hay_deadlock_exceptuando_al_siguiente(entrenadores unEquipo, matriz_recursos necesidad, matriz_recursos disponibles, entrenadores omitidos, entrenadores enDeadlock){
+
+	entrenadores procesos = unEquipo;
+	entrenadores procesosOmitidos = list_duplicate(omitidos);
+
+	entrenador*unEntrenador = list_remove(procesos, 0);
+
+	entrenadores aEvaluar = list_duplicate(procesos);
+	list_add_all(aEvaluar, procesosOmitidos);
+
+	if(!unEntrenador){
+		puts("L-NULL");
+		return false;
+	}
+
+	printf("L%d\n", unEntrenador->id);
+
+	matriz_recursos necesidadActual = recursos_matriz_diferencia(necesidad, entrenador_recursos_pedidos(unEntrenador));
+	matriz_recursos disponiblesActuales = recursos_matriz_diferencia(disponibles, entrenador_recursos_sobrantes(unEntrenador));
+
+
+	bool esperaCircular = hay_espera_circular(aEvaluar, necesidadActual, disponiblesActuales);
+
+	if(esperaCircular){
+		list_add_all(enDeadlock, aEvaluar);
+	}
+
+	bool deadlock = esperaCircular
+				 || hay_deadlock_exceptuando_al_siguiente(procesos, necesidadActual, disponiblesActuales, procesosOmitidos, enDeadlock)
+			     || hay_deadlock_dejando_al_siguiente(procesos, necesidadActual, disponiblesActuales, procesosOmitidos, enDeadlock);
+
+	list_add_in_index(procesos, 0, unEntrenador);
+
+	recursos_destroy(necesidadActual);
+	recursos_destroy(disponiblesActuales);
+	list_destroy(aEvaluar);
+
+	return deadlock;
+}
+
+bool hay_deadlock_dejando_al_siguiente(entrenadores unEquipo, matriz_recursos necesidad, matriz_recursos disponibles, entrenadores omitidos, entrenadores enDeadlock){
+
+	entrenadores procesos = unEquipo;//esAca list_duplicate(unEquipo);
+	entrenadores procesosOmitidos = list_duplicate(omitidos);
+
+	entrenador*unEntrenador = list_remove(procesos, 0);
+	list_add(procesosOmitidos, unEntrenador);
+
+	if(!unEntrenador){
+//		puts("D-NULL");
+		return false;
+	}
+
+	printf("D%d\n", unEntrenador->id);
+
+	entrenadores aEvaluar = list_duplicate(procesos);
+	list_add_all(aEvaluar, procesosOmitidos);
+
+	bool esperaCircular = hay_espera_circular(aEvaluar, necesidad, disponibles);
+
+	if(esperaCircular){
+		list_add_all(enDeadlock, aEvaluar);
+	}
+
+	bool deadlock = esperaCircular
+		|| hay_deadlock_exceptuando_al_siguiente(procesos, necesidad, disponibles, procesosOmitidos, enDeadlock)
+		|| hay_deadlock_dejando_al_siguiente(procesos, necesidad, disponibles, procesosOmitidos, enDeadlock);
+
+
+	//esAca
+	list_add_in_index(procesos, 0, unEntrenador);
+
+	return deadlock;
+}
+
+bool hay_espera_circular(entrenadores unEquipo, matriz_recursos necesidad,matriz_recursos disponibles){
+//	printf("Necesidad:"); recursos_mostrar(necesidad); puts("");
+//	printf("Disponibles:"); recursos_mostrar(disponibles);puts("");
+	return recursos_suficientes_para(necesidad, disponibles) && list_size(unEquipo)>1;
+}
+
 ////*************************************** Intercambios ************************************************//
 //void algoritmo_procesar_deadlock(entrenadores unEquipo){
 //	//TODO
@@ -169,40 +182,40 @@ void team_ejecutar_algoritmo_de_deteccion_de_deadlock(){
 
 
 ////*************************************** Auxiliares *********************************************
-//matriz_recursos entrenador_recursos_sobrantes(entrenador*unEntrenador){
-//	return recursos_matriz_diferencia_positiva(unEntrenador->pokemonesCazados, unEntrenador->objetivos);
-//}
-//
-////retorna los objetivos no cumplidos del entrenador, o sea, la "necesidad particular"
-//matriz_recursos entrenador_recursos_pedidos(entrenador*unEntrenador){
-//	return recursos_matriz_diferencia_positiva(unEntrenador->objetivos, unEntrenador->pokemonesCazados);
-//}
-//
-//matriz_recursos entrenadores_pedidos_insatisfechos(entrenadores unEquipo){
-//	matriz_recursos pedidos = recursos_create();
-//
-//	void sumarInsatisfechos(entrenador*unEntrenador){
-//		matriz_recursos necesidad = entrenador_recursos_pedidos(unEntrenador); //Se va a ir con nueva estructura
-//		recursos_sumar_recursos_a(pedidos, necesidad);
-//		recursos_destroy(necesidad);
-//	}
-//
-//	list_iterate(unEquipo, (void(*)(void*))&sumarInsatisfechos);
-//
-//	return pedidos;
-//}
-//
-//matriz_recursos entrenadores_recursos_sobrantes(entrenadores unEquipo){
-//	matriz_recursos sobrantes = recursos_create();
-//
-//	void sumarInsatisfechos(entrenador*unEntrenador){
-//		matriz_recursos disponibles = entrenador_recursos_sobrantes(unEntrenador); //Se va a ir con nueva estructura
-//		recursos_sumar_recursos_a(sobrantes, disponibles);
-//		recursos_destroy(disponibles);
-//	}
-//
-//	list_iterate(unEquipo, (void(*)(void*))&sumarInsatisfechos);
-//
-//	return sobrantes;
-//
-//}
+matriz_recursos entrenador_recursos_sobrantes(entrenador*unEntrenador){
+	return recursos_matriz_diferencia_positiva(unEntrenador->pokemonesCazados, unEntrenador->objetivos);
+}
+
+//retorna los objetivos no cumplidos del entrenador, o sea, la "necesidad particular"
+matriz_recursos entrenador_recursos_pedidos(entrenador*unEntrenador){
+	return recursos_matriz_diferencia_positiva(unEntrenador->objetivos, unEntrenador->pokemonesCazados);
+}
+
+matriz_recursos entrenadores_pedidos_insatisfechos(entrenadores unEquipo){
+	matriz_recursos pedidos = recursos_create();
+
+	void sumarInsatisfechos(entrenador*unEntrenador){
+		matriz_recursos necesidad = entrenador_recursos_pedidos(unEntrenador); //Se va a ir con nueva estructura
+		recursos_sumar_recursos_a(pedidos, necesidad);
+		recursos_destroy(necesidad);
+	}
+
+	list_iterate(unEquipo, (void(*)(void*))&sumarInsatisfechos);
+
+	return pedidos;
+}
+
+matriz_recursos entrenadores_recursos_sobrantes(entrenadores unEquipo){
+	matriz_recursos sobrantes = recursos_create();
+
+	void sumarInsatisfechos(entrenador*unEntrenador){
+		matriz_recursos disponibles = entrenador_recursos_sobrantes(unEntrenador); //Se va a ir con nueva estructura
+		recursos_sumar_recursos_a(sobrantes, disponibles);
+		recursos_destroy(disponibles);
+	}
+
+	list_iterate(unEquipo, (void(*)(void*))&sumarInsatisfechos);
+
+	return sobrantes;
+
+}
--- Team/src/hilos del team/detector de deadlock.h
+++ Team/src/hilos del team/detector de deadlock.h
@@ -9,22 +9,23 @@
 // * cambiar los nombres de las funciones para que sean mas acordes a los fines
 // * En teoria los entrenadores que retornan, llegan en orden de ejecucion, con lo cual con que yo remueva dos en orden deberian poder intercambiar
 // */
-//
+
 bool algoritmo_detectar_deadlock(entrenadores candidatos, entrenadores enDeadlock);
-//
-//void algoritmo_procesar_deadlock(entrenadores entrenadoresEnDeadlock);
-//
-//bool hay_deadlock_exceptuando_al_siguiente(entrenadores unEquipo, matriz_recursos necesidad, matriz_recursos disponibles, entrenadores enDeadlock);
-//
-//bool hay_deadlock_dejando_al_siguiente(entrenadores unEquipo, matriz_recursos necesidad, matriz_recursos disponibles, entrenadores enDeadlock);
-//
-//bool hay_espera_circular(entrenadores unEquipo, matriz_recursos necesidad,matriz_recursos disponibles);
-//
-//matriz_recursos entrenador_recursos_sobrantes(entrenador*);
-//
-//matriz_recursos entrenador_recursos_pedidos(entrenador*);
-//
-//matriz_recursos entrenadores_pedidos_insatisfechos(entrenadores unEquipo);
-//matriz_recursos entrenadores_recursos_sobrantes(entrenadores unEquipo);
-//
+
+void algoritmo_procesar_deadlock(entrenadores entrenadoresEnDeadlock);
+
+bool hay_deadlock_exceptuando_al_siguiente(entrenadores unEquipo, matriz_recursos necesidad, matriz_recursos disponibles, entrenadores omitidos, entrenadores enDeadlock);
+
+bool hay_deadlock_dejando_al_siguiente(entrenadores unEquipo, matriz_recursos necesidad, matriz_recursos disponibles, entrenadores omitidos, entrenadores enDeadlock);
+
+bool hay_espera_circular(entrenadores unEquipo, matriz_recursos necesidad,matriz_recursos disponibles);
+
+matriz_recursos entrenador_recursos_sobrantes(entrenador*);
+
+matriz_recursos entrenador_recursos_pedidos(entrenador*);
+
+matriz_recursos entrenadores_pedidos_insatisfechos(entrenadores unEquipo);
+
+matriz_recursos entrenadores_recursos_sobrantes(entrenadores unEquipo);
+
 #endif
