183ffe3c8f0a5a2225eb4c68f454aae264ddb30d
GustavoChoque <e_gustavo14@hotmail.com>
2020-06-09 10:17:24 -0300

    quito prueba de envio de paquete de gameboy

--- GameBoy/src/GameBoy.c
+++ GameBoy/src/GameBoy.c
@@ -17,30 +17,10 @@ int main(void) {
 	//Me traigo la config para conocer la ip puerto del broker
 	t_config* config = config_create("./config/gameboy.config");
 
-	/*conexion_broker = conexion_server_crear(
+	conexion_broker = conexion_server_crear(
 				config_get_string_value(config, "IP_BROKER"),
 				config_get_string_value(config, "PUERTO_BROKER"), GAMEBOY,
-				0);*/
-
-	t_conexion_server* unaConexion=conexion_server_crear(
-			config_get_string_value(config, "IP_GAMECARD"),
-			config_get_string_value(config, "PUERTO_GAMECARD"), GAMEBOY);
-
-
-
-
-		t_mensaje_new_pokemon* mensajeAenviar=mensaje_new_pokemon_crear("pikachu",2,3,10);
-
-		t_paquete_header header=paquete_header_crear(MENSAJE,GAMEBOY,NEW_POKEMON);
-
-		t_buffer* bufferDepaquete=mensaje_new_pokemon_serializar(mensajeAenviar);
-
-		t_paquete* paqueteAEnviar=paquete_crear(header,bufferDepaquete);
-
-
-		enviar(unaConexion,paqueteAEnviar);
-
-
+				0);
 
 	return 1;
 
