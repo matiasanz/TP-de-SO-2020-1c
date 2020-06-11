b2fa87951259e4f4de157550bdee68a95e1a0783
matiasanz <matias.l.anzorandia@gmail.com>
2020-06-11 12:38:14 -0300

    Team no se autorresponde mensaje CATCH

--- Team/src/estructuras-auxiliares/mensajes.c
+++ Team/src/estructuras-auxiliares/mensajes.c
@@ -8,6 +8,27 @@ void* leer_mensaje_cuando_este_disponible(cr_list* unaLista){
 void Get(void* especiePokemon) {
 	//Envia mensaje al broker para ser replicado al gamecard
 	printf("get(%s), ", (especie_pokemon) especiePokemon);
+
+//	t_mensaje_get_pokemon* mensajeGet=mensaje_get_pokemon_crear(string_duplicate(especiePokemon));
+//
+//	t_paquete_header header=paquete_header_crear(MENSAJE,TEAM,GET_POKEMON);
+//	t_buffer* bufferDepaquete=mensaje_get_pokemon_serializar(mensajeGet);
+//	t_paquete* paqueteAEnviar=paquete_crear(header,bufferDepaquete);
+//	int resultadoDeEnvio = enviar(conexion_broker,paqueteAEnviar);
+
+//TODO t_id* idGet = malloc(sizeof(t_id)); *idGet = mensaje_get_pokemon_get_id(mensajeGet); list_add(getsPendientes, idGet); //y funcion que compare en localized
+
+//	if(resultadoDeEnvio==ERROR_SOCKET){
+//		pthread_mutex_lock(&Mutex_AndoLoggeandoEventos);
+//		log_warning(event_logger,"Se procedera a responder el mensaje GET por defecto");
+//		pthread_mutex_unlock(&Mutex_AndoLoggeandoEventos);
+//		t_mensaje_localized_pokemon* respuestaAutogenerada = mensaje_localized_pokemon_crear(string_duplicate(especiePokemon), list_create());
+//		cr_list_add_and_signal(mensajesLOCALIZED, respuestaAutogenerada);
+//	}
+
+//	mensaje_get_pokemon_destruir(mensajeGet);
+//	paquete_destruir(paqueteAEnviar);
+
 //	log_info(event_logger, ">> get(%s)\n", (especie_pokemon) especiePokemon);
 }
 
@@ -15,13 +36,12 @@ void Get(void* especiePokemon) {
 void Get_pokemones(matriz_recursos pokemones){
 
 	void unGetPorPokemon(char* unaEspecie, void*cantidad){
+		//sleep(Recargo_CPU); //Cada get consume 1 cpu
 		Get(unaEspecie);
 	}
 
 	dictionary_iterator(pokemones, unGetPorPokemon); puts("");
 }
-
-t_id unIDhardcodeado=0;
 
 //Envia mensaje al broker para ser replicado al gamecard, devuelve el id del mensaje pendiente por recibir
 void Catch(entrenador*unEntrenador, pokemon* pokemonCatcheado) {
@@ -46,7 +66,7 @@ void Catch(entrenador*unEntrenador, pokemon* pokemonCatcheado) {
 
 //	if(resultadoDeEnvio==ERROR_SOCKET){
 //		pthread_mutex_lock(&Mutex_AndoLoggeandoEventos);
-//		log_warning(event_logger,"Se procedera a responder el mensaje por defecto");
+//		log_warning(event_logger,"Se procedera a responder el mensaje CATCH por defecto");
 //		pthread_mutex_unlock(&Mutex_AndoLoggeandoEventos);
 //
 ////		t_mensaje_caught_pokemon* respuestaAutogenerada = mensaje_caught_pokemon_crear(true);
