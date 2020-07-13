a4c9e6d32b322db04f6b302890bf87bf4faca259
matiasanz <matias.l.anzorandia@gmail.com>
2020-07-12 23:57:35 -0300

    des/habilitar logs del broker por config

--- Broker/src/Broker.c
+++ Broker/src/Broker.c
@@ -54,8 +54,9 @@ void inicializar_config() {
 void inicializar_logs() {
 
 	char* ruta = config_get_string_value(config, "LOGGER");
-	logger = log_create(ruta, BROKER_STRING, 1, LOG_LEVEL_INFO);
-	event_logger = log_create("./log/broker_event.log", "BROKER_EVENT", 1, LOG_LEVEL_INFO);
+	MOSTRAR_LOGS = config_get_int_value(config, "MOSTRAR_LOGS");
+	logger = log_create(ruta, BROKER_STRING, MOSTRAR_LOGS, LOG_LEVEL_INFO);
+	event_logger = log_create("./log/broker_event.log", "BROKER_EVENT", MOSTRAR_LOGS, LOG_LEVEL_INFO);
 }
 
 void atender_cliente(int* socket) {
--- Broker/src/broker.h
+++ Broker/src/broker.h
@@ -7,6 +7,7 @@
 
 pthread_t hilo_receptor_mensajes;
 int socket_servidor;
+bool MOSTRAR_LOGS;
 
 void atender_cliente(int* socket);
 void inicializar();
