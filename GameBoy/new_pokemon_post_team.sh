37ec6d4169ec6d0556cc6a96461a9f6458f158f8
matiasanz <matias.l.anzorandia@gmail.com>
2020-07-18 11:18:53 -0300

    Agrego scripts de prueba

---
+++ GameBoy/new_pokemon_post_team.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+./gameboy BROKER APPEARED_POKEMON Onix 2 8 0
+./gameboy BROKER APPEARED_POKEMON Gengar 6 6 0
+./gameboy BROKER APPEARED_POKEMON Squirtle 5 5 0
+
+./gameboy BROKER APPEARED_POKEMON Umbreon 10 6 0
+./gameboy BROKER APPEARED_POKEMON Espeon 7 1 0
+./gameboy BROKER APPEARED_POKEMON Vaporeon 4 10 0
